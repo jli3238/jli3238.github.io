be4ff9fe282762bb2565f44d33b15e52e4d0a1b2
jli3238 <jli3238@gmail.com>
2020-06-10 11:17:28 -0500

    Simplify fetch data from API's and handling of errors

--- playground/src/ES6Promise.jsx
+++ playground/src/ES6Promise.jsx
@@ -4,54 +4,33 @@ import PageContainer from '../src/PageContainer';
 const ES6Promise = () => {
     
   const [rates, setRates] = useState([]);
-  const [ratesError, setRatesError] = useState('');
-  const [areRatesLoaded, setAreRatesLoaded] = useState(false);
   const ratesApiUrl = 'data/rates.json';
   const rateRankApiUrl = 'data/rateRank.json';
   useEffect(() => {
     try {
       fetch(ratesApiUrl)
-        .then(response1 => { 
-          if (response1.ok) {
-            return response1.json(); 
-          } else { 
-            throw Error(response1.statusText); 
-          }})
+        .then(response1 => response1.json())
         .then(
           result1 => {
             fetch(rateRankApiUrl)
-              .then(response2 => {
-                if (response2.ok) {
-                  return response2.json();
-                } else { 
-                  throw Error(response2.statusText); 
-                }})
+              .then(response2 => response2.json())
               .then(
                 result2 => {
-                  setAreRatesLoaded(true);
                   const results = result1.map(res1 => { return { ...res1, rank: result2.find(res2 => res1.name === res2.name).rank }; });
                   setRates(results);
                 },
-                error2 => {
-                  setRatesError(error2);
-                }
+                error2 => console.log(error2)
               )
           },
-          error1 => {
-            setRatesError(error1);
-          }
+          error1 => console.log(error1)
         )
-    } catch (error) {
-      setRatesError(error);
-    }
+    } catch (error) { console.log(error); }
   }, []);
 
   return (
     <PageContainer 
         title='Using ES6 Promise'
         description='Show fetch data using ES6 promise.'>        
-        { ratesError !== '' && <div>{ratesError}</div>}
-        { areRatesLoaded && <div>Loaded</div>}
         <div>Rates are as follows: </div> 
         <table className="rates-table">
             <thead><tr><th>Name</th><th>Years</th><th>Rate</th><th>Rank</th></tr></thead>
